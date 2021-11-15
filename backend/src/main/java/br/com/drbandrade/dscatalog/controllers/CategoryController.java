954318e94428bad8b208d911dc198ea135401652
dRbAndrade <chucreandrade@gmail.com>
2021-11-15 00:20:11 -0300

    DB Seeding

--- backend/src/main/java/br/com/drbandrade/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/br/com/drbandrade/dscatalog/controllers/CategoryController.java
@@ -4,6 +4,7 @@ import br.com.drbandrade.dscatalog.entities.Category;
 import br.com.drbandrade.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class CategoryController {
     private CategoryService service;
 
     @GetMapping
+    @Transactional(readOnly = true)
     public ResponseEntity<List<Category>> findAll(){
         List<Category> list = service.findAll();
         return ResponseEntity.ok().body(list);
